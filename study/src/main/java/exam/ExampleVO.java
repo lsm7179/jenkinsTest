dc3cb93685f88335a626272925dbec62f17374ac
이승민 <@DESKTOP-63ECQ2C>
2020-06-10 13:18:52 +0900

    로컬에 커밋하기

---
+++ study/src/main/java/exam/ExampleVO.java
@@ -0,0 +1,41 @@
+package exam;
+
+public class ExampleVO {
+	private String Word;
+	private String Explanation;
+	private String Subject_Main;
+	private String Subject_Middle;
+	private String Subject_Smaill;
+	public String getWord() {
+		return Word;
+	}
+	public void setWord(String word) {
+		Word = word;
+	}
+	public String getExplanation() {
+		return Explanation;
+	}
+	public void setExplanation(String explanation) {
+		Explanation = explanation;
+	}
+	public String getSubject_Main() {
+		return Subject_Main;
+	}
+	public void setSubject_Main(String subject_Main) {
+		Subject_Main = subject_Main;
+	}
+	public String getSubject_Middle() {
+		return Subject_Middle;
+	}
+	public void setSubject_Middle(String subject_Middle) {
+		Subject_Middle = subject_Middle;
+	}
+	public String getSubject_Smaill() {
+		return Subject_Smaill;
+	}
+	public void setSubject_Smaill(String subject_Smaill) {
+		Subject_Smaill = subject_Smaill;
+	}
+	
+	
+}
